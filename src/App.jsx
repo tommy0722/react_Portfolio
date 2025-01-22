eaf6f395f2bd64e0380211ce3d1ec1eeed68641f
Tommy <t40201@gmail.com>
2025-01-23 00:35:09 +0800

    css 箭頭函數教學

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import './App.css'
+
+function Mycomponent() {
+  return <h1>你好 </h1>
+}
+
 
 function App() {
-
-
+  const text = 'hello world!!!'
+  const handleClick=() =>{
+    alert('測試箭頭函數')
+  }
   return (
-    <div>hello world</div>
+    <>
+    <button className='my5' onClick={function(){alert('你好')}}>我是按紐</button>
+    <br />
+    <button className='my5' onClick={handleClick}>我是按紐 箭頭函數</button>
+    <br />
+    <input type="text" placeholder={text} />
+      <h1 className="h1-tag" style={{backgroundColor:'red'}} >{text.toUpperCase()}</h1>
+      <div><Mycomponent /></div>
+    </>
   )
 }
 
