2691700bf44b2ef245101d918586e1dc2a2be244
Tommy <t40201@gmail.com>
2025-01-23 00:39:31 +0800

    陣列

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,14 @@ function Mycomponent() {
 
 
 function App() {
-  const text = 'hello world!!!'
-  const handleClick=() =>{
-    alert('測試箭頭函數')
-  }
+  const listItems=[
+    <Mycomponent key='1' />,
+    <Mycomponent key='2' />,
+    <Mycomponent key='3' />,
+  ]
   return (
     <>
-    <button className='my5' onClick={function(){alert('你好')}}>我是按紐</button>
-    <br />
-    <button className='my5' onClick={handleClick}>我是按紐 箭頭函數</button>
-    <br />
-    <input type="text" placeholder={text} />
-      <h1 className="h1-tag" style={{backgroundColor:'red'}} >{text.toUpperCase()}</h1>
-      <div><Mycomponent /></div>
+      {listItems}
     </>
   )
 }
