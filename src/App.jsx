c00298fa0059807413dc1051cd09675541cfc5e9
Tommy <t40201@gmail.com>
2025-01-23 00:44:22 +0800

    filter 跟 map用法

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import './App.css'
 
 function Mycomponent() {
-  return <h1>你好 </h1>
+	return <h1>你好 </h1>
 }
 
 
 function App() {
-  const listItems=[
-    <Mycomponent key='1' />,
-    <Mycomponent key='2' />,
-    <Mycomponent key='3' />,
-  ]
-  return (
-    <>
-      {listItems}
-    </>
-  )
+	const listItems = [
+		{ content: '張三', id: 'abc' },
+		{ content: '李四', id: 'qaz' },
+		{ content: '老五', id: 'wsx' },
+	]
+	const filterItems = listItems.filter((item) =>{
+		if (item.content !== '李四'){
+			return true
+		}
+	})
+	return (
+		<>
+			{listItems.map((item) => {
+				return <div key={item.id}>{item.content}</div>
+			})}
+			{filterItems.map((item) => {
+				return <div key={item.id}>{item.content}</div>
+			})}
+		</>
+	)
 }
 
 export default App
