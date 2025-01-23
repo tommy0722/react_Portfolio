7217e323cb92349a3ec4f53da9cdc39414722949
Tommy <t40201@gmail.com>
2025-01-23 09:04:34 +0800

    prop附值

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import './App.css'
 
+function Mycomponent({a,b='王曉明',c}) {
+	c()
+	return <>
+		<div>{a}</div>
+		<div>{b}</div>
+	</>
+}
 
 
 function App() {
 	return (
-		<div>
-			<div className={true ? 'a c' : 'b c'}>
-				{true ? <h1>hello</h1> : <h1>world</h1>}
-			</div>
-			<div className={`c ${false ? 'a' : 'b'}`}>
-				{true ? <h1>hello</h1> : <h1>world</h1>}
-			</div>
-		</div>
+		<>
+			<Mycomponent a="hello world" c={()=>console.log(123)} 
+				
+				/>
+		</>
 	)
-
 }
 
 export default App
