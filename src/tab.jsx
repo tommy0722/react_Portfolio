d3a9bb0e31f413adb18f756c6e010d55e176b982
Tommy <t40201@gmail.com>
2025-02-11 02:03:49 +0800

    tab

---
+++ src/tab.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { Tabs, Tab } from 'react-bootstrap';
+import MyCard from './components/myCard';
+import image1 from './assets/Frieren.jpg';
+import image2 from './assets/Pwclolr.jpg';
+import image3 from './assets/Roulette.jpg';
+
+function ContentTabs() {
+    const cssData = [
+        {
+            title: "色塊拼圖",
+            text: "這是一個放兩張圖片，透過滑動來解開第二張圖片的動畫",
+            image: image1,
+            link: "/JigsawArt",
+            buttonText: "連結",
+        },
+    ];
+    const jsData = [
+        {
+            title: "密碼強度判斷",
+            text: "確認密碼強度的顏色牆",
+            image: image2,
+            link: "/pwColor",
+            buttonText: "連結",
+        },
+    ];
+    const backendData = [
+        {
+            title: "輪盤系統",
+            text: "今天吃甚麼的輪盤(外部連結)",
+            image: image3,
+            link: "https://myweb-backend-571409330129.asia-east1.run.app",
+            buttonText: "連結",
+        },
+    ];
+
+    return (
+        <div className='container'>
+            <p>現在沒有確定的作品集，只是先亂放</p>
+            <Tabs defaultActiveKey="css" className="mb-3">
+                <Tab eventKey="css" title="CSS">
+                    <div style={{ padding: '20px' }}>
+                        {cssData.map((card, index) => (
+                            <MyCard key={index} data={card} />
+                        ))}
+                    </div>
+                </Tab>
+                <Tab eventKey="js" title="JS">
+                    <div style={{ padding: '20px' }}>
+                        {jsData.map((card, index) => (
+                            <MyCard key={index} data={card} />
+                        ))}
+                    </div>
+                </Tab>
+                <Tab eventKey="backend" title="後端">
+                    <div style={{ padding: '20px' }}>
+                        {backendData.map((card, index) => (
+                            <MyCard key={index} data={card} />
+                        ))}
+                    </div>
+                </Tab>
+            </Tabs>
+        </div>
+    );
+}
+
+export default ContentTabs;
